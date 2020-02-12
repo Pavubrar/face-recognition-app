17be8956b03781924478f35bec1aab058755bb33
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2020-02-12 14:17:35 -0800

    adding file with api

--- src/App.js
+++ src/App.js
@@ -1,39 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'tachyons';
+import Clarifai from 'clarifai';
 import Navigation from './components/navigation/navigation';
 import Logo from "./components/logo/logo";
 import ImageLinkForm from "./components/imageLink/imageLinkForm";
 import Rank from "./components/rank/rank";
+import FaceRecognition from './components/faceRecognition/faceRecognition';
 import './App.css';
-import Particles  from 'react-particles-js'
-const particlesOptions ={
+import Particles from 'react-particles-js'
+
+const app = new Clarifai.App({
+  apiKey: 'c554fc5b7b9e427dbe94aa5f639f489d'
+});
+const particlesOptions = {
   particles: {
-    
-      number:{
-value: 30,
-density:{
 
-  enable: true,
-  value_area:200
+    number: {
+      value: 30,
+      density: {
 
-}
+        enable: true,
+        value_area: 200
+
       }
-    
-}
-} 
+    }
 
-function App() {
-  return (
-    <div className="App">
-    <Particles className='particles'
-                params={particlesOptions} />
-     <Navigation />
-      <Logo />
-      <Rank />
-     <ImageLinkForm />
-     {/* <FaceRecognitionForm /> */}
-    </div>
-  );
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: ''
+    }
+  }
+  onInputChange = (event) => {
+    
+this.setState({input: event.target.value})
+console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict( 
+      Clarifai.COLOR_MODEL,this.state.input)
+      .then(
+      function (response) {
+        console.log('click')
+        console.log(response)
+      },
+      function (err) {
+       console.log(err)
+      }
+    );
+  }
+  render() {
+
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    )
+  }
 }
 
 export default App;
